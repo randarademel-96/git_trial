eb56f8f1590e15b5681433bc2a20b22921de04a3
Randara <rmgdemel@gmail.com>
2025-11-24 10:36:25 +0530

    First commit

---
+++ src/main/java/com/git_trial/trial/TrialApplication.java
@@ -0,0 +1,13 @@
+package com.git_trial.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_trial/trial/TrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_trial.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
